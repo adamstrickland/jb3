ecd7567b849688adf7c4f982aeac77a1972a65e8
Adam Strickland <adam.strickland@gmail.com>
2012-07-26 15:52:25 -0500

    new rails app + postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jb3::Application.config.secret_token = '64534055184d6980e812f49bcb2f6e4b2906138a1000e469a4da1119ff1b1e0a1c8e17971029e9d106625a88d34ca7214fbfb94dff732f1073713569a85e3743'
